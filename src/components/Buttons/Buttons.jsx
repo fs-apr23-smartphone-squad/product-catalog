14064464adaaa5847b5c39d40b8f0ea979935aeb
Volodymyr Petruliak <106393188+snuber-w@users.noreply.github.com>
2023-07-31 14:57:07 +0300

    add TS

---
+++ src/components/Buttons/Buttons.jsx
@@ -0,0 +1,24 @@
+export const Buttons = () => (
+  <div class="buttons">
+    <div class="icon--menu-wrapper">
+      <a
+        href="#menu"
+        class="icon icon--menu"
+      ></a>
+    </div>
+
+    <div class="icon-wrapper">
+      <a
+        href="#favourites"
+        class="icon icon--favourites"
+      ></a>
+    </div>
+
+    <div class="icon-wrapper">
+      <a
+        href="#cart"
+        class="icon icon--cart"
+      ></a>
+    </div>
+  </div>
+);
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,50 @@
+import { Buttons } from '../Buttons';
+
+export const Header = () => (
+    // <header class="header">
+    //   <div class="header__left">
+    //     <div class="logo">
+    //       <a href="#home">
+    //         <img
+    //           class="logo__img"
+    //           src="images/logo.svg"
+    //           alt="Nice gadgets logo"
+    //         >
+    //         </img>
+    //       </a>
+    //     </div>
+    //     <nav class="nav">
+    //       <ul class="nav__list">
+    //         <li class="nav__item">
+    //           <a class="nav__link is-active" href="#home">
+    //             home
+    //           </a>
+    //         </li>
+
+    //         <li class="nav__item">
+    //           <a class="nav__link" href="#phones">
+    //             phones
+    //           </a>
+    //         </li>
+
+    //         <li class="nav__item">
+    //           <a class="nav__link" href="#tablets">
+    //             tablets
+    //           </a>
+    //         </li>
+
+    //         <li class="nav__item">
+    //           <a
+    //             class="nav__link"
+    //             href="#accessories"
+    //           >
+    //           accessories
+    //           </a>
+    //         </li>
+    //       </ul>
+    //     </nav>
+    //   </div>
+
+      <Buttons />
+    // </header>
+);
