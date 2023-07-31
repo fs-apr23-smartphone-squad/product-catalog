29eceea69390a6d20b4aac71e17889e554d2967c
Iryna Balandiukh <balandiukh.i@gmail.com>
2023-07-31 15:57:51 +0300

    Add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-// import logo from './logo.svg';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
 import './App.css';
 import { Header } from './components/Header';
+import { HomePage } from './pages/HomePage';
+import { PhonesPage } from './pages/PhonesPage';
+import { TabletsPage } from './pages/TabletsPage';
+import { AccessoriesPage } from './pages/AccessoriesPage';
 
 function App() {
   return (
     <div className="App">
       <Header />
+
+      <Routes>
+          <Route path="home" element={<HomePage/>} />
+          <Route path="/" element={<Navigate to="home" />} />
+          <Route path="phones" element={<PhonesPage />} />
+          <Route path="tablets" element={<TabletsPage />} />
+          <Route path="accessories" element={<AccessoriesPage />} />
+          <Route path="*" element={<h1 className="title">Page not found</h1>} />
+        </Routes>
     </div>
   );
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,50 +1,52 @@
+import { Link } from 'react-router-dom';
+
 import { Buttons } from '../Buttons';
 
 export const Header = () => (
-    // <header class="header">
-    //   <div class="header__left">
-    //     <div class="logo">
-    //       <a href="#home">
-    //         <img
-    //           class="logo__img"
-    //           src="images/logo.svg"
-    //           alt="Nice gadgets logo"
-    //         >
-    //         </img>
-    //       </a>
-    //     </div>
-    //     <nav class="nav">
-    //       <ul class="nav__list">
-    //         <li class="nav__item">
-    //           <a class="nav__link is-active" href="#home">
-    //             home
-    //           </a>
-    //         </li>
+  <header className="header">
+    <div className="header__left">
+      <div className="logo">
+        <Link to="home">
+          <img
+            className="logo__img"
+            src="images/logo.svg"
+            alt="Nice gadgets logo"
+          >
+          </img>
+        </Link>
+      </div>
+      <nav className="nav">
+        <ul className="nav__list">
+          <li className="nav__item">
+            <Link className="nav__link is-active" to="home">
+              home
+            </Link>
+          </li>
 
-    //         <li class="nav__item">
-    //           <a class="nav__link" href="#phones">
-    //             phones
-    //           </a>
-    //         </li>
+          <li className="nav__item">
+            <Link className="nav__link" to="phones">
+              phones
+            </Link>
+          </li>
 
-    //         <li class="nav__item">
-    //           <a class="nav__link" href="#tablets">
-    //             tablets
-    //           </a>
-    //         </li>
+          <li className="nav__item">
+            <Link className="nav__link" to="tablets">
+              tablets
+            </Link>
+          </li>
 
-    //         <li class="nav__item">
-    //           <a
-    //             class="nav__link"
-    //             href="#accessories"
-    //           >
-    //           accessories
-    //           </a>
-    //         </li>
-    //       </ul>
-    //     </nav>
-    //   </div>
+          <li className="nav__item">
+            <Link
+              className="nav__link"
+              to="accessories"
+            >
+            accessories
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
 
       <Buttons />
-    // </header>
+  </header>
 );
