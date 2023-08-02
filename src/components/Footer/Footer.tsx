d5111114183e164c61ce47e680ba7d7aeafc8565
Andrii Mashutikov <mashutikov.a.o@gmail.com>
2023-08-02 09:19:03 +0300

    fixed img issue

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,16 +1,13 @@
-import { Contacts } from '../Contacts';
-import './footer.scss';
-import logo from '../../images/logo.svg';
+import { Contacts } from "../Contacts";
+import "./footer.scss";
+import logo from "../../images/logo.svg";
 
 export const Footer = () => (
   <footer className="footer">
     <div className="footer__container">
       <div className="footer__logo">
         <a href="#">
-          <img
-            src={logo}
-            alt="Nice gadgets logo"
-          />
+          <img src={`${logo}`} alt="Nice gadgets logo" />
         </a>
       </div>
 
@@ -18,7 +15,6 @@ export const Footer = () => (
 
       <a className="footer__back-to-top-btn" href="#">
         Back to top
-
         <div className="footer__back-to-top-btn-icon"></div>
       </a>
     </div>
