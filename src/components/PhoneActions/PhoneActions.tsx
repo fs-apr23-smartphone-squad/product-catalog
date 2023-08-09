9a7d3adab8417477bcc4fc83baa9e12e327a34ea
Volodymyr Petruliak <106393188+snuber-w@users.noreply.github.com>
2023-08-09 12:11:54 +0300

    fast fix

--- src/components/PhoneActions/PhoneActions.tsx
+++ src/components/PhoneActions/PhoneActions.tsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import cn from 'classnames';
-import React, { useEffect, useState } from 'react';
-import cn from 'classnames';
 import './PhoneActions.scss';
 import { NavLink } from 'react-router-dom';
 import {
