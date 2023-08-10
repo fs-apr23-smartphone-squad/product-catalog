071e01bb3f6b1a5d5523b5c1b73ad362ff163272
Volodymyr Petruliak <106393188+snuber-w@users.noreply.github.com>
2023-08-10 21:17:26 +0300

    Update HomePage.tsx

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -100,7 +100,7 @@ export const HomePage: React.FC<Props> = ({
           phoneIdsInFavourites={phoneIdsInFavourites}
           handleAddToFavourites={handleAddToFavourites}
           removeFromFavourites={removeFromFavourites}
-          title='Hot priceRegulars'
+          title='Hot price'
         />
       </div>
     </div>
